32ede684d260511ed34aa988f9003ac8324f34b8
Swapnil-Patil-04 <swapnil.sp4444@gmail.com>
2024-05-09 15:28:26 +0530

    Update main.py

--- Task 2-Simple Calculator/main.py
+++ Task 2-Simple Calculator/main.py
@@ -1,6 +1,5 @@
-# Design a simple calculator with basic arithmetic operations.
-# Prompt the user to input two numbers and an operation choice.
-# Perform the calculation and display the result.
+# Simple calculator 
+# Swapnil Patil
 
 from termcolor import colored
 
