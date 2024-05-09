5ecf3b715344ff90a0a7c3befde0f59eb60f7f5b
Swapnil-Patil-04 <swapnil.sp4444@gmail.com>
2024-05-09 15:27:45 +0530

    Update main.py

--- Task 1-To Do List Manager/main.py
+++ Task 1-To Do List Manager/main.py
@@ -1,7 +1,6 @@
 # TASK - 1
-# A To-Do List application is a useful project that helps users manage and organize their tasks efficiently.
-# This project aims to create a command-line or GUI-based application using Python, allowing users to create, update,
-# and track their to-do lists.
+# A To-Do List application
+# Swapnil Patil
 
 from tkinter import *
 import tkinter.messagebox as msg
